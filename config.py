b491260e1278596f7d5e520f322eefac55cfec9c
godmon19392id <godsuraj564@gmail.com>
2025-01-25 11:02:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "adbbccf8eed67602e3c13f2524272ae6")
     BOT_TOKEN = environ.get("BOT_TOKEN", "7762159903:AAGLo582RUp5mI3fywXjowWPOXna7I2xQhU") 
     BOT_SESSION = environ.get("BOT_SESSION", "Evecrgrg) 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://godsuraj564:Rb9HKktUHIN5qAnD@cluster0.p5pp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "godsuraj564")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
